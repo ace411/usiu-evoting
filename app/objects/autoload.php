a5d9cf06f59094f013ac2ea4f9fbbfe0630c6638
ace411 <lochbm@live.com>
2016-07-28 16:00:14 +0300

    Update autoload.php

--- app/objects/autoload.php
+++ app/objects/autoload.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ *Autoload the classes in the objects folder
+ *No namespaces are loaded along with the files
+ *PSR-4-compliant system to follow soon
+*/
+
 spl_autoload_register(function($className){
 	$view_files = htmlspecialchars(dirname(__DIR__));
 	$view_files = str_replace("\\", "/", $view_files);
